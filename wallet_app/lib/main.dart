06f7fa40550410f4384330e63cb84a107184ad2d
Hayatuden <orhanbeym123@gmail.com>
2025-12-09 00:40:09 +0300

    cards

--- wallet_app/lib/main.dart
+++ wallet_app/lib/main.dart
@@ -6,33 +6,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      title: Text('FLutter Demo'),
       home: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.menu, color: Colors.white),
-          title: Text('Flutter Demo Home Page', style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.deepPurple,
+          title: Text('Melaverse'),
         ),
-        body: Stack(
-          children: [
-            Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
-            Center(
-              child: Text('Welcome to Melaverse', style: TextStyle(color: Colors.white)),
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Orhan'),
-              trailing: Icon(Icons.arrow_forward),
-              onTap: () {
-                print('clicked');
-              },
-            ),
-          ],
+        body: Container(
+          
         ),
       ),
     );
