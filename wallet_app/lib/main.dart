22e8dae0bd0a0cfcdc247951da0f99b83baf0988
Hayatuden <orhanbeym123@gmail.com>
2025-12-09 00:32:42 +0300

    Bakcground images

--- wallet_app/lib/main.dart
+++ wallet_app/lib/main.dart
@@ -14,46 +14,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       home: Scaffold(
         appBar: AppBar(
+          leading: Icon(Icons.menu, color: Colors.white),
           title: Text('Flutter Demo Home Page', style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.deepPurple,
         ),
-        body: Image.network(
-          'https://img.freepik.com/free-photo/anime-style-character-with-fire-flames_23-2151152391.jpg?t=st=1765224148~exp=1765227748~hmac=69c5e701cdea635adab261addc4a407ea97df1b026a8d8f019b34f81e85f6a57&w=740',
-          height: double.infinity,
-          fit: BoxFit.cover,
+        body: Stack(
+          children: [
+            Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
+            Center(
+              child: Text('Welcome to Melaverse', style: TextStyle(color: Colors.white)),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Orhan'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('clicked');
+              },
+            ),
+          ],
         ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text(widget.title)),
-      body: Center(child: Text('Hello')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
