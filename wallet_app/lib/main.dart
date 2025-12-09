e8d7c69dfb6f1cea95663c3b53e5a57524dd68d5
Hayatuden <orhanbeym123@gmail.com>
2025-12-09 10:46:42 +0300

    rebuild the app

--- wallet_app/lib/main.dart
+++ wallet_app/lib/main.dart
@@ -4,16 +4,28 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// Material APP(stateful)
+// scaffold
+// app title
+// bottom navbar setState
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal)),
-      title: 'FLutter Demo',
-      home: Scaffold(),
-    );
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),),
+      home: Scaffold(
+        appBar: AppBar(
+
+        ),
+    )
   }
 }
