d338531c4c809b7b5fa443906e997e86ffd6a6b5
Hayatuden <orhanbeym123@gmail.com>
2025-12-10 21:43:12 +0300

    lottie package

--- wallet_app/lib/views/pages/home_page.dart
+++ wallet_app/lib/views/pages/home_page.dart
@@ -8,7 +8,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(20.0),
-      child: Column(children: [HeroWidget()]),
+      child: Column(
+        children: [
+          HeroWidget(),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: Card(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(children: [Text('Welcome to melaverse!'), Text('This is where inovation meets reality')]),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
